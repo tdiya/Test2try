9e689f5b10909e46a28bb18432eddbd5c434aa32
Tahmina Sultana <Tahmina Sultana@192.168.0.12>
2019-10-09 13:38:22 -0400

    Added a class in this project named "Git2test"

---
+++ gitMytry/src/gitMytry/Git2test.java
@@ -0,0 +1,13 @@
+package gitMytry;
+
+public class Git2test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String projectName = ("gitMytry");
+		System.out.println(projectName);
+
+	}
+
+}
